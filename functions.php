8c09a664e135bde769667961987e5f05e903715d
Julian Andrade <julianchaos@gmail.com>
2016-02-04 01:27:15 -0200

    Carregamento do jQuery e do Bootstrap, pelo functions.php

--- functions.php
+++ functions.php
@@ -7,3 +7,19 @@ function register_my_menu() {
 }
 add_action( 'after_setup_theme', 'register_my_menu' );
 
+function ceres_scripts_styles() {
+	$templateDirectory = get_template_directory_uri();
+	
+	//jQuery
+	wp_enqueue_script( 'jquery');
+	
+	/*
+	 * Bootstrap
+	 * url: http://getbootstrap.com/
+	 */
+	$bootstrapDistDirectory = $templateDirectory . "/vendor/twitter/bootstrap/dist";
+	wp_enqueue_script( 'ceres-bootstrap', $bootstrapDistDirectory . "/js/bootstrap.min.js" , array('jquery'), '3.3.6', false);
+	wp_enqueue_style( 'ceres-bootstrap', $bootstrapDistDirectory . "/css/bootstrap.min.css" , array(), '3.3.6');
+}
+add_action( 'wp_enqueue_scripts', 'ceres_scripts_styles' );
+

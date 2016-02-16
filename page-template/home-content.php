8e74a84458a17791adb2c82fd56eb0a78f219c89
Julian Andrade <julianchaos@gmail.com>
2016-02-16 00:17:30 -0200

    Conteúdo da home

---
+++ page-template/home-content.php
@@ -0,0 +1,35 @@
+<section id="home-content">
+	<div class="container">
+		<div class="row">
+			<div id="home-content-main" class="col-xs-12 col-sm-8 content-main">
+				<div class="row">
+					<div id="home-content-main-description" class="col-xs-12">
+						<h1 class="text-uppercase"><?php the_title() ?></h1>
+						<div class="text-justify">
+							<?php the_content() ?>
+						</div>
+					</div>
+					<div class="col-xs-12">
+						<h1 class="separator-top">Conheça também</h1>
+					</div>
+					<div class="col-xs-12 col-sm-6 noticia">
+						<img src="<?php bloginfo('template_directory') ?>/images/home/foto-noticia.png" alt="Notícia" class="img-responsive center-block" />
+						<h4>Nullam non efficitur erat. Cras non nisi vitae ante ornare suscipit.</h4>
+						<p><small class="orange">11 de outubro de 2015 | <span class="orange dark">Projetos</span></small></p>
+						<p>Fusce venenatis venenatis vestibulum. Donec eu molestie enim. Nullam non efficitur erat. Cras non nisi vitae ante ornare suscipit sed a dolor. Sed dignissim risus leo, id convallis erat blandit vitae.</p>
+						<p><a href="javascript: void(0)" class="orange dark">Leia mais</a></p>
+					</div>
+					<div class="col-xs-12 col-sm-6 noticia">
+						<img src="<?php bloginfo('template_directory') ?>/images/home/foto-noticia.png" alt="Notícia" class="img-responsive center-block" />
+						<h4>Nullam non efficitur erat. Cras non nisi vitae ante ornare suscipit.</h4>
+						<p><small class="orange">11 de outubro de 2015 | <span class="orange dark">Projetos</span></small></p>
+						<p>Fusce venenatis venenatis vestibulum. Donec eu molestie enim. Nullam non efficitur erat. Cras non nisi vitae ante ornare suscipit sed a dolor. Sed dignissim risus leo, id convallis erat blandit vitae.</p>
+					</div>
+				</div>
+			</div>
+			<div id="home-content-widget" class="social-widget col-xs-12 col-sm-4">
+				<?php get_template_part('page-template/social') ?>
+			</div>
+		</div>
+	</div>
+</section>
--- page-template/page_home.php
+++ page-template/page_home.php
@@ -3,5 +3,7 @@
  * Template Name: Home
  */
 get_header();
+the_post();
 get_template_part('page-template/home', 'carousel');
+get_template_part('page-template/home', 'content');
 get_footer();

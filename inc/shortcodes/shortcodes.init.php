8928a8869e52c91701e92158360c39d7af95aa8c
Julian Andrade <julianchaos@gmail.com>
2016-02-20 17:39:21 -0200

    Listagem dinâmica das notícias do widget social

--- inc/shortcodes/shortcodes.init.php
+++ inc/shortcodes/shortcodes.init.php
@@ -17,6 +17,37 @@ function printSidebarSocialContent()
 	$sidebar = ob_get_contents();
 	ob_end_clean();
 	
+	$sidebar .= printSocialContentNews();
+	
 	return $sidebar;
 }
 add_shortcode('print_sidebar_social_content', 'printSidebarSocialContent');
+
+function printSocialContentNews()
+{
+	$query = new WP_Query(array( 'post_type' => 'post', 'post_status' => 'publish', 'posts_per_page' => 3 ));
+
+	$output = null;
+	if($query->have_posts())
+	{
+		while($query->have_posts())
+		{
+			$query->the_post();
+
+			$noticia = array(
+				'date' => get_the_date('j \d\e F \d\e Y'),
+				'content' => get_the_excerpt(),
+				'link' => get_permalink()
+			);
+			
+			$output .= "<div class='separator-bottom'>"
+						. "<p>{$noticia['content']}</p>"
+						. "<p class='date'><small class='orange'>{$noticia['date']}</small></p>"
+					. "</div>";
+		}
+	}
+	
+	wp_reset_postdata();
+	
+	return $output;
+}

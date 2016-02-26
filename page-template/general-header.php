8e9ed0cc52214807129760cae133c11692ad5c62
Julian Andrade <julianchaos@gmail.com>
2016-02-26 18:18:48 -0300

    Correção na apresentação do título das páginas internas

--- page-template/general-header.php
+++ page-template/general-header.php
@@ -1,11 +1,8 @@
-<?php
-$title = (!isset($title) || $title === '' ? wp_title('') : $title);
-?>
 <section id="general-header">
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12">
-				<h1 class="text-uppercase"><?php echo $title ?></h1>
+				<h1 class="text-uppercase"><?php if ( !isset($title) || ($title === '')){ wp_title(''); } else {echo $title;} ?></h1>
 			</div>
 		</div>
 	</div>

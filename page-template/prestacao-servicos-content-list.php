248f2792ea1e410d3d03c5087736a4586c28d6d6
Julian Andrade <julianchaos@gmail.com>
2016-03-08 19:32:49 -0300

    Listagem de detalhes das páginas Prestação de serviços e padronização da largura (margem) dos ícones font awesome

--- page-template/prestacao-servicos-content-list.php
+++ page-template/prestacao-servicos-content-list.php
@@ -12,9 +12,21 @@ while($query->have_posts())
 {
 	$query->the_post();
 	
+	$endereco = get_post_meta($post->ID, 'endereco', true);
+	$data = get_post_meta($post->ID, 'data', true);
+	$valor = get_post_meta($post->ID, 'valor', true);
+	$telefone = get_post_meta($post->ID, 'telefone', true);
+	$observacao = get_post_meta($post->ID, 'observacao', true);
+	
 	$lista[] = array(
 		'titulo' => get_the_title(),
-		'descricao' => get_the_content()
+		'descricao' => get_the_content(),
+		
+		'endereco' => $endereco,
+		'data' => $data,
+		'valor' => $valor,
+		'telefone' => $telefone,
+		'observacao' => $observacao
 	);
 }
 ?>
@@ -29,7 +41,12 @@ foreach($lista as $item)
 				<div class="separator-bottom">
 					<div class="prestacao-servico-content item-content">
 						<h4><?php echo $item['titulo'] ?></h4>
-						<?php echo $item['descricao'] ?>
+						<p><?php echo $item['descricao'] ?></p>
+						<p><i class="fa fa-map-marker"></i><?php echo $item['endereco'] ?></p>
+						<p><i class="fa fa-calendar"></i><?php echo $item['data'] ?></p>
+						<p><i class="fa fa-money"></i><?php echo $item['valor'] ?></p>
+						<p><i class="fa fa-phone"></i><?php echo $item['telefone'] ?></p>
+						<p><?php echo $item['observacao'] ?></p>
 					</div>
 				</div>
 			</div>

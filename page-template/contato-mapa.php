2751def95e4b26f4361c10ab754f39b509a2d4de
Julian Andrade <julianchaos@gmail.com>
2016-03-11 13:39:32 -0300

    Seção do mapa da página contato atendendo à todos os breakpoints

--- page-template/contato-mapa.php
+++ page-template/contato-mapa.php
@@ -1,8 +1,25 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<section id="contato-mapa" class="interna-content">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<p>Prefere nos fazer uma visita?</p>
+			</div>
+			<div class="col-xs-12">
+				<a href="">
+					<img src="<?php bloginfo('template_directory') ?>/images/contato/mapa.png" class="img-responsive" alt="" />
+				</a>
+				<div class="mapa-content">
+					<p class="endereco">
+						<i class="fa fa-map-marker"></i> <?php the_field('endereco') ?>
+					</p>
+					<p class="telefone">
+						<i class="fa fa-phone"></i> Tel.: <?php the_field('telefone') ?>
+					</p>
+					<p class="fax">
+						<i class="fa fa-fax"></i> Fax: <?php the_field('fax') ?>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>

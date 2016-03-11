53a7dc62c93b7646be4d7cf71811819157b8b0b3
Julian Andrade <julianchaos@gmail.com>
2016-03-11 11:43:35 -0300

    Template da página de Contato com seção do formulário de contato atendendo à todos os breakpoints

---
+++ page-template/contato-formulario.php
@@ -0,0 +1,11 @@
+<section id="contato-formulario" class="interna-content">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 separator-bottom">
+				<div class="row">
+					<?php echo do_shortcode('[contact-form-7 id="116" title="Contato"]') ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
---
+++ page-template/page_contato.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Template Name: Contato
+ */
+get_header();
+
+get_template_part('page-template/general', 'header');
+get_template_part('page-template/contato', 'formulario');
+get_template_part('page-template/contato', 'mapa');
+
+get_footer();

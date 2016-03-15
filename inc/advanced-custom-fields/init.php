359a0cc915e6c1ae4fbc2dca8343e1c83725e595
Julian Andrade <julianchaos@gmail.com>
2016-03-15 20:00:43 -0300

    Advanced Custom Fields definido na versão lite (não apresenta informações do plugin na área administrativa)

--- inc/advanced-custom-fields/init.php
+++ inc/advanced-custom-fields/init.php
@@ -1,3 +1,3 @@
-<?php define( 'ACF_LITE', false ) ?>
+<?php define( 'ACF_LITE', true ) ?>
 <?php require_once 'advanced-custom-fields/acf.php' ?>
 <?php require_once 'advanced-custom-fields-contact-form-7-field/acf-cf7.php' ?>

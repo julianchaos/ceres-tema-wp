97bef0ff950ebac8cd93c3716bb8ac639b77fed5
Julian Andrade <julianchaos@gmail.com>
2016-04-11 11:45:20 -0300

    Campos personalizados para telefone e email de pesquisadores

--- page-template/pesquisadores-content-list.php
+++ page-template/pesquisadores-content-list.php
@@ -18,13 +18,18 @@ while($query->have_posts())
 	$thumb = (is_array($thumbArray) ? $thumbArray[0] : $templateURI . '/images/pesquisadores/blank-picture.png');
 	$linkedin = get_post_meta($post->ID, 'linkedin', true);
 	$cur_lattes = get_post_meta($post->ID, 'curriculo_lattes', true);
+	$email = get_post_meta($post->ID, 'email', true);
+	$telefone = get_post_meta($post->ID, 'telefone', true);
 	
 	$lista[] = array(
 		'thumb' => $thumb,
 		'titulo' => get_the_title(),
-		'descricao' => get_the_content(),
+		'descricao' => "<p>" . get_the_content() . "</p>",
 		'linkedin' => (strlen($linkedin) > 0 ? "<a href='$linkedin' class='linkedin'><img src='{$templateURI}/images/pesquisadores/linkedin.png' alt='' ></a>" : null),
-		'curriculo-lattes' => (strlen($cur_lattes) > 0 ? "<a href='$cur_lattes' class='curriculo-lattes'><img src='{$templateURI}/images/pesquisadores/curriculo-lattes.png' alt='' ></a>" : null)
+		'curriculo-lattes' => (strlen($cur_lattes) > 0 ? "<a href='$cur_lattes' class='curriculo-lattes'><img src='{$templateURI}/images/pesquisadores/curriculo-lattes.png' alt='' ></a>" : null),
+		'email' => (strlen($email) > 0 ? "<p><i class='fa fa-envelope'></i>{$email}</p>" : null),
+		'telefone' => (strlen($telefone) > 0 ? "<p><i class='fa fa-phone'></i>{$telefone}</p>" : null)
+				
 	);
 }
 ?>
@@ -47,6 +52,8 @@ foreach($lista as $item)
 							echo $item['curriculo-lattes'];
 						} ?></h4>
 						<?php echo $item['descricao'] ?>
+						<?php echo $item['telefone'] ?>
+						<?php echo $item['email'] ?>
 					</div>
 				</div>
 			</div>

6dd5f663850e6e902602ca0467f07b76f88afad3
Julian Andrade <julianchaos@gmail.com>
2016-04-11 12:58:38 -0300

    Adição o ACF Repeater

--- inc/advanced-custom-fields/init.php
+++ inc/advanced-custom-fields/init.php
@@ -1,3 +1,4 @@
 <?php define( 'ACF_LITE', true ) ?>
 <?php require_once 'advanced-custom-fields/acf.php' ?>
 <?php require_once 'advanced-custom-fields-contact-form-7-field/acf-cf7.php' ?>
+<?php require_once 'acf-repeater/acf-repeater.php' ?>

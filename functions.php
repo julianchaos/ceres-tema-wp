cd56656c252a6481aa8c52efe3802a9d382039b5
Julian Andrade <julianchaos@gmail.com>
2016-06-20 17:31:16 -0300

    A listagem de notícias apresenta agora 3 notícias por página.

--- functions.php
+++ functions.php
@@ -60,7 +60,7 @@ function alter_main_query($query)
 {
 	if( $query->is_main_query() && !(is_admin()) )
 	{
-		$query->set('posts_per_page', 2);
+		$query->set('posts_per_page', 3);
 	}
 }
 add_action('pre_get_posts', 'alter_main_query');
